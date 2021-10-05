0514334c74700fd44461b51a783020a015555d7d
bviraktamath <mvbasu92@gmail.com>
2021-10-05 10:00:45 +0530

    second

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,10 +1,38 @@
 <template>
-    <h1>weather Dashboard</h1>
+  <div>
+    <h1>Api Data</h1>
+
+    <div v-for="post in posts" :key="post.id">
+      <p>{{ post.id }}, {{ post.title }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'Weather',
- 
-}
+  name: "Weather",
+
+  created() {
+      this.getposts()
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    getposts() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => {
+          console.log(res.data);
+          this.posts = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
